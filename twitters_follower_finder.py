23b6982bd7bf606a86ce7e9a45458fc998ef416b
Khandoker Tanjim Ahammad <63535729+Khandoker09@users.noreply.github.com>
2022-05-17 16:41:01 +0200

    update the script
    - to dl xlsx file

--- twitters_follower_finder.py
+++ twitters_follower_finder.py
@@ -14,31 +14,32 @@ import datetime
 # for creating the file name
 current_date = datetime.datetime.now()
 #all the api keys from the developer accounts 
-consumer_key = "key"
-consumer_secret = "key"
-access_key = "key"
-access_secret = "key"
+consumer_key = "IL16kNKNhp7aOgDobPGcmpQrt"
+consumer_secret = "gOevdNkCMDVYMZPf04K8NBHwPewyTWXdh3VZcyMKL9EpDd61NF"
+access_key = "1316280381570772997-SYLFOS1m5zjevhWeDFbvCRiOh1fJqd"
+access_secret = "09w7OyTSt7MolBO2U5HbXQOb66SVyGOaTTSvFziIYLH33"
 # Pass your twitter credentials to tweepy via its OAuthHandler
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 # Creation of query for finding the followers . parameter = screen_nmae
-followers = tweepy.Cursor(api.get_followers, screen_name="airtablestatus").items(200)  
-
+screen_name="ButterCMS"
+followers = tweepy.Cursor(api.get_followers, screen_name="ButterCMS",tweet_mode="extended",count=5).items(2)
+while True:
 # Pulling information from tweets iterable object
-try:
-# Add or remove tweet information you want in the below list comprehension
-    followers_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in followers]
-# Creation of dataframe from tweets_list
-except:
-    followers_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in followers]
-    time.sleep(15)
-# Did not include column names to simplify code 
+    try:
+    # Add or remove tweet information you want in the below list comprehension
+        followers_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in followers]
+    # Creation of dataframe from tweets_list
+    except tweepy.TweepyException:
+        followers_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in followers]
+        time.sleep(15)
+      
+    # Did not include column names to simplify code 
 followers_list_df = pd.DataFrame(followers_list)
 followers_list_df.columns = ['username','user_screenname','userid','userslocation','usersurl','usersdescription','userverified','followerscount','friendscount','statusescount','listedcount','createdat','profileimageurlhttps','defaultrofile']
 
-filename = str('twitter_followers_list_')+str(current_date.year)+str('_')+str(current_date.month)+str('_')+str(current_date.day)
+filename = str('_followers_list_')+str(current_date.year)+str('_')+str(current_date.month)+str('_')+str(current_date.day)
 print(followers_list_df)
-followers_list_df.to_csv(str(filename + '.csv'),index=False)
-#followers_list_df.to_excel(str(filename + '.csv'),index=False)
+followers_list_df.to_excel(str(screen_name+filename + '.xlsx'),index=False)
